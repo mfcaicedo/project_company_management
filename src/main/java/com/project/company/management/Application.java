15745dda80864e133be36106167ecd30e9a57427
mfcaicedo <mfcaicedo@unicauca.edu.co>
2022-08-19 20:01:42 -0500

    projectCompanyManagement: Modificando el principal

--- src/main/java/com/project/company/management/Application.java
+++ src/main/java/com/project/company/management/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("hi");
+		System.out.println("Hola soy Milthon F Caicedo Jurado - mfcaicedo@unicauca.edu.co");
 	}
 
 }
