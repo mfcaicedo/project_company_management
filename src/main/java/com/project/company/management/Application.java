6570f0a63887107f7017269a9c8ab931b9f23701
mfcaicedo <mfcaicedo@unicauca.edu.co>
2022-08-21 20:09:34 -0500

    Hola mundo con Sprinboot

--- src/main/java/com/project/company/management/Application.java
+++ src/main/java/com/project/company/management/Application.java
@@ -2,23 +2,14 @@ package com.project.company.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("hi");
 		System.out.println("Hola soy Milthon F Caicedo Jurado - mfcaicedo@unicauca.edu.co");
-	}
-	
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
 	}
 
 }
