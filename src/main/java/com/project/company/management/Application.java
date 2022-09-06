5a9ca328b16cc31e97c8b757b2e84727a25751ec
Javierramirezm01 <79489896+Javierramirezm01@users.noreply.github.com>
2022-09-06 13:04:49 -0500

    prueba de spring

--- src/main/java/com/project/company/management/Application.java
+++ src/main/java/com/project/company/management/Application.java
@@ -35,7 +35,7 @@ public class Application {
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+	return String.format("hello %s!", name);
 	}
 
 }
